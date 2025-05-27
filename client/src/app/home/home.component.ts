d478bfd6856d4be6285f974100800b8a075cacab
nadine-993 <n.kantarji993@gmail.com>
2025-05-27 17:39:07 +0400

    Authentican fiished

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { LoginComponent } from "../login/login.component";
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [LoginComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent  {
+
+ 
+
+}
---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,47 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { AccountService } from '../_services/account.service';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+  http= inject (HttpClient);
+  users:any;
+  ngOnInit(): void {
+    this.getUsers()
+  }
+  accountService = inject(AccountService);
+  model: any= {};
+  login(){
+    this.accountService.login(this.model).subscribe({
+      next: response =>  {
+        console.log(this.model);
+
+        console.log(response);
+      },
+      error: error => console.log(error)
+    })
+  }
+  logout(){
+    this.accountService.logout();
+  }
+  getUsers(){
+    this.http.get('http://localhost:5091/api/users').subscribe({
+      next: response=> this.users=response,
+
+      error:error=> console.log(error),
+      complete:()=> console.log(this.users)
+    })
+  }
+
+}
